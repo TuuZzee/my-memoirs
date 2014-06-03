a6a866f8942282510f4238510a8fc4998e912ebf
becker-g <becker.g.ept@gmail.com>
2014-06-03 10:54:39 +0900

    add some feature to sign-up

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name     "Guillaume Becker"
     email    "guillaume.becker@yopmail.com"
-    password "M0t2p@ss"
-    password_confirmation "M0t2p@ss"
+    password "mot2pass"
+    password_confirmation "mot2pass"
   end
 end
