c7eeabbe42065f5394080b92c7107b0deec820ee
becker-g <becker.g.ept@gmail.com>
2014-06-16 23:20:25 +0900

    Some refacto and update the gemfile

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -85,10 +85,10 @@ describe "User pages" do
 
    describe "with valid information" do
       before do
-        fill_in "Name",         with: "Boby Lapointe"
-        fill_in "Email",        with: "boby.lapointe@yopmail.com"
-        fill_in "Password",     with: "mot2pass"
-        fill_in "Confirmation", with: "mot2pass"
+        fill_in "Name",             with: "Boby Lapointe"
+        fill_in "Email",            with: "boby.lapointe@yopmail.com"
+        fill_in "Password",         with: "mot2pass"
+        fill_in "Confirm Password", with: "mot2pass"
       end
 
       it "should create a user" do
@@ -149,6 +149,18 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
